d1467a53c2ad9a48dfebbf8278492670b2918591
kagort <kagort@yandex.ru>
2025-03-14 15:24:15 +0300

    140325

--- queries/extract_sentences.py
+++ queries/extract_sentences.py
@@ -59,5 +59,5 @@ def extract_sentences_with_tokens(output_file):
 
 # Вызов функции
 if __name__ == '__main__':
-    output_csv_file = '../Datasets_from_pgAdmin/sentences_with_tokens.csv'
+    output_csv_file = '../Datasets_from_pgAdmin/simple_sentences.csv'
     extract_sentences_with_tokens(output_csv_file)
