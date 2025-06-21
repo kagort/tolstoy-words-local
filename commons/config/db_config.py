20078e22b0fd6a945f2f920e584e207daedbae57
kagort <kagort@yandex.ru>
2025-06-21 21:27:37 +0300

    feat: def cleaning duplicates

--- commons/config/db_config.py
+++ commons/config/db_config.py
@@ -2,7 +2,7 @@ from os import environ
 from sqlalchemy import create_engine
 
 DB_USER = environ.get('DB_USER', 'postgres')
-DB_PASSWORD = environ.get('DB_PASSWORD', '123')
+DB_PASSWORD = environ.get('DB_PASSWORD', 'ouganda77')
 DB_HOST = environ.get('DB_HOST', 'localhost')
 DB_PORT = environ.get('DB_PORT', '5432')
 DB_NAME_GENERIC = environ.get('DB_NAME', 'generic')
