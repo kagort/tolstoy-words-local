1cf4be70db46eae3b47f83d3642af9d635c32854
parshintseva <parshintsevaao@baltech.ru>
2025-09-08 00:05:25 +0300

    chore: db config changed to cloud

--- commons/config/db_config.py
+++ commons/config/db_config.py
@@ -1,12 +1,12 @@
 from os import environ
 from sqlalchemy import create_engine
 
-DB_USER = environ.get('DB_USER', 'postgres')
-DB_PASSWORD = environ.get('DB_PASSWORD', 'ouganda77')
-DB_HOST = environ.get('DB_HOST', 'localhost')
+DB_USER = environ.get('DB_USER', 'admin')
+DB_PASSWORD = environ.get('DB_PASSWORD', 'password')
+DB_HOST = environ.get('DB_HOST', 'pgadmin.quotverba.ru')
 DB_PORT = environ.get('DB_PORT', '5432')
 DB_NAME_GENERIC = environ.get('DB_NAME', 'generic')
-DB_NAME_NATURAL = environ.get('DB_NAME', 'tolstoy_words_csv')
+DB_NAME_NATURAL = environ.get('DB_NAME', 'natural')
 
 engine_generic = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME_GENERIC}')
 engine_natural = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME_NATURAL}')
